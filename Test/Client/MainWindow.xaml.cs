6b4313f9690daaf430c4c9dbbe228e00a8d09c92
Iinsooya <125941993+Iinsooya@users.noreply.github.com>
2024-07-18 19:57:57 +0700

    Add files via upload

---
+++ Test/Client/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Client
+{
+    /// <summary>
+    /// Логика взаимодействия для Win1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TcpClient client = null;
+        NetworkStream stream = null;
+        public string username;
+        public int port = 45454;
+        public int Count;
+        public string address = "127.0.0.1";
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        void listen()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] data = new byte[64];
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+                    string message = builder.ToString();
+                    Dispatcher.BeginInvoke(new Action(() => Clientlog.Items.Add(message)));
+                }
+            }
+            catch (Exception ex)
+            {
+                Clientlog.Items.Add(ex.Message);
+            }
+        }
+
+        private void Connect_Click(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                username = Name.Text;
+                client = new TcpClient();
+                stream = client.GetStream();
+                MessageBox.Show("Подключено к серверу.");
+
+                byte[] receivedData = new byte[4]; // Предполагается, что счетчик представлен 4 байтами
+                stream.Read(receivedData, 0, receivedData.Length);
+                int counter = BitConverter.ToInt32(receivedData, 0);
+
+                string[] parts = address.Split('.');
+                int lastNumber = int.Parse(parts[parts.Length - 1]);
+                int newLastNumber = lastNumber + counter;
+                parts[parts.Length - 1] = newLastNumber.ToString();
+                string Add = string.Join(".", parts);
+                
+                
+                
+
+
+
+
+                Thread listenThread = new Thread(() => listen());
+
+                // Сделать занесение при подключении данных о клиенте (Имя, адрес, порт)
+
+
+
+                listenThread.Start();
+               
+                //byte[] name = Encoding.Unicode.GetBytes(Name.Text);
+                byte[] count = Encoding.Unicode.GetBytes(Add.ToString());
+                // stream.Write(data, 0, data.Length);
+                //stream.Write(name, 0, name.Length);
+                stream.Write(count, 0, count.Length);
+
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.BeginInvoke(new Action(() => Clientlog.Items.Add("Ошибка: " + ex.Message)));
+            }
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            string message = Mes.Text;
+            if (!string.IsNullOrEmpty(Address.Text))
+            {
+                message = String.Format("{0} to {1}: {2}", username, Address.Text, message);
+            }
+            else
+            {
+                message = String.Format("{0}: {1}", username, message);
+            }
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+        }
+
+
+        private void Disconnect_Click(object sender, RoutedEventArgs e)
+        {
+            string mes = "Отключился от сервера";
+            mes = String.Format("{0}: {1}", username, mes);
+            byte[] data = Encoding.Unicode.GetBytes(mes);
+            stream.Write(data, 0, data.Length);
+            MessageBox.Show("Отключено от сервера.");
+        }
+
+       
+    }
+}
---
+++ Test/Server-Client/MainWindow.xaml.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Server_Client
+{
+    public class ClientInfo
+    {
+        public string IPAddress { get; set; }
+        public int Port { get; set; }
+        public string Name { get; set; }
+    }
+
+    public partial class MainWindow : Window
+    {
+        private TcpListener listener;
+        private int port = 45454;
+        private string name;
+        int Count = 0;
+        private ObservableCollection<string> connectedClients = new ObservableCollection<string>();
+        private List<TcpClient> connectedTcpClients = new List<TcpClient>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            log.ItemsSource = connectedClients;
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+            Thread listenThread = new Thread(Listen);
+            listenThread.Start();
+            Dispatcher.Invoke(() => connectedClients.Add("Сервер включен."));
+        }
+
+
+        void Listen()
+        {   
+
+            try
+            {
+                listener.Start();
+                bool isServerStarted = true;
+
+                while (isServerStarted)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    NetworkStream stream = client.GetStream();
+                    IPEndPoint clientEndPoint = (IPEndPoint)client.Client.RemoteEndPoint;
+                    ClientInfo clientInfo = new ClientInfo
+                    {
+                        IPAddress = clientEndPoint.Address.ToString(),
+                        Port = 45454,
+                        Name = name
+                    };
+                    connectedTcpClients.Add(client); // Store the connected client
+                    Count = Count + 1;
+                    byte[] data = BitConverter.GetBytes(Count);
+                    stream.Write(data, 0, data.Length);
+                    Dispatcher.BeginInvoke(new Action(() => connectedClients.Add("New client connected.")));
+
+                    Thread clientThread = new Thread(() => Process(client));
+                    clientThread.Start();
+                    
+
+                }
+            }
+            catch (SocketException ex)
+            {
+                // Handle the exception and provide feedback to the user
+                Dispatcher.BeginInvoke(new Action(() => connectedClients.Add("Error occurred: " + ex.Message)));
+            }
+            finally
+            {
+                // Ensure that the listener is stopped when the server is no longer running
+                if (listener != null)
+                {
+                    listener.Stop();
+                }
+            }
+        }
+
+        public async Task SendMessageAsync(TcpClient client, string message)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            await stream.WriteAsync(data, 0, data.Length);
+        }
+
+        public void Process(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream stream = null;
+
+            try
+            {
+                stream = tcpClient.GetStream();
+                byte[] data = new byte[64];
+
+                while (tcpClient.Connected)
+                {
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+                    string recipientTcpClient = ExtractRecipient(message);
+
+                    Dispatcher.BeginInvoke(new Action(() => connectedClients.Add(message)));
+
+                    // Send the message to all connected clients
+                    byte[] responseData = Encoding.Unicode.GetBytes(message);
+                    byte[] recipientData = Encoding.Unicode.GetBytes(recipientTcpClient);
+                    foreach (var connectedClient in connectedTcpClients)
+                    {
+                        
+                        // Другие свойства клиента, которые вы хотите вывести
+                        Console.WriteLine();
+
+                        NetworkStream clientStream = connectedClient.GetStream();
+                        //NetworkStream recipientStream = connectedTcpClients.FirstOrDefault(selectedClient => selectedClient.Name == recipientTcpClient)?.GetStream();
+                        if (string.IsNullOrEmpty(recipientTcpClient))
+                        {
+                            clientStream.Write(responseData, 0, responseData.Length);
+                        }
+                        else {
+                            clientStream.Write(recipientData, 0, recipientData.Length);
+                            //clientStream.Write(responseData, 0, responseData.Length); // Отправка сообщения только адресату
+                            
+                        }
+                        // Check if the recipient is in the list of connected clients
+
+                    }
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Dispatcher.BeginInvoke(new Action(() => connectedClients.Add(ex.Message)));
+            }
+            finally
+            {
+                stream?.Close();
+                tcpClient.Close();
+            }
+        }
+
+        private string ExtractRecipient(string message)
+        {
+            int recipientIndex = message.IndexOf(" to ");
+            if (recipientIndex != -1)
+            {
+                int recipientEndIndex = message.IndexOf(":", recipientIndex);
+                if (recipientEndIndex != -1)
+                {
+                    return message.Substring(recipientIndex + 4, recipientEndIndex - (recipientIndex + 4)).Trim();
+                }
+            }
+            return string.Empty;
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action(() => connectedClients.Add("Сервер выключен.")));
+            listener.Stop();
+        }
+    }
+}
